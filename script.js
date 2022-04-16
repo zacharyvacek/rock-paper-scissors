ff473745d351913282e3048e40f0d1b6d7f06273
Zachary Vacek <zacharyvacek@gmail.com>
2022-04-16 08:48:15 -0500

    create function for computer selection

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 // computerSelection Function returns either Rock, Paper, or Scissors
 
+function computerSelection() {
+    const computerChoice = Math.floor(Math.random() * 3);
+    let selection;
+    // console.log(computerChoice);
+    if (computerChoice === 0) {
+        selection = "rock";
+    } else if (computerChoice === 1) {
+        selection = "paper";
+    } else {
+        selection = "scissors";
+    };
+    return selection;
+}
+
+console.log(computerSelection());
+
 // playerSelection function
     // prompts the user for their selection and returns their selection.
 
