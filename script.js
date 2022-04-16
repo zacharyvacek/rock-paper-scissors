2329c37f4a77dadc9e80c3278027b46d4ad67ac7
Zachary Vacek <zacharyvacek@gmail.com>
2022-04-16 08:38:54 -0500

    write pseudocode for game

---
+++ script.js
@@ -0,0 +1,10 @@
+// computerSelection Function returns either Rock, Paper, or Scissors
+
+// playerSelection function
+    // prompts the user for their selection and returns their selection.
+
+// rpsRound function takes computerSelection and playerSelection argument
+    //returns string that declares the winner
+
+// game function
+    //loops through five rounds and keeps score of the winner
