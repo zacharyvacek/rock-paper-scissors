7219116bd6f14fbd9908fb275d6e6cb1e4d6a917
zacharyvacek <zacharyvacek@gmail.com>
2025-05-22 03:46:09 -0500

    create getComputerChoice function

--- game.js
+++ game.js
@@ -1,1 +1,18 @@
-console.log("Hello, World!");
+function getComputerChoice() {
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * max);
+    }
+    const computerChoice = getRandomInt(3);
+    let choice;
+    if (computerChoice == 0) {
+        choice = "Rock";
+    } else if (computerChoice == 1) {
+        choice = "Paper";
+    } else {
+        choice = "Scissors";
+    }
+    return choice;
+}
+
+console.log(getComputerChoice());
+
